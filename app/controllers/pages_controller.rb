fa050d3c8977666cdc419107494d18e3dd27cbd9
Ka Mok <heykamok@gmail.com>
2016-09-25 00:49:36 -0400

    Add basic presentation for articles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ require 'json'
 class PagesController < ApplicationController
   def home
     @user = User.fetch_or_create(request.remote_ip)
-    @content = Content.new(@user.profile_id).generate
+    @articles = Content.new(@user.profile_id).generate.articles
     #Remember that content has articles, profile_id, and theme
   end
 end
