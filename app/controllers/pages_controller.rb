cbd8eedc845dd794d9d7a4224a4db24c2294b20d
Ka Mok <heykamok@gmail.com>
2016-09-24 20:33:53 -0400

    Add content class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,6 @@ require 'json'
 class PagesController < ApplicationController
   def home
     @user = User.fetch_or_create(request.remote_ip)
+    @content = Content.new(@user.profile_id).generate
   end
 end
---
+++ app/models/content.rb
@@ -0,0 +1,38 @@
+require 'open-uri'
+require 'json'
+
+class Content
+  attr_reader :profile_id, :theme, :articles
+
+  def initialize(profile_id)
+    @profile_id = profile_id
+  end
+
+  def generate
+    content_data = get_content_data
+
+    set_theme(content_data)
+    generate_articles(content_data)
+    return self
+  end
+
+  private
+
+  def get_content_data
+    base_url = "https://peaceful-springs-7920.herokuapp.com/content/"
+    JSON.parse(open(base_url + "#{@profile_id}/").read)
+  end
+
+  def set_theme(content_data)
+    @theme = content_data["theme"]
+  end
+
+  def generate_articles(content_data)
+    @articles = []
+    content_data["articles"].each do |article|
+      p article
+    end
+  end
+end
+
+p Content.new(4).generate
