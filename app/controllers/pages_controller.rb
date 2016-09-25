c2e1b41eb1a73f66dfd666cffae696327d3f83c6
Ka Mok <heykamok@gmail.com>
2016-09-25 01:04:18 -0400

    Add the css conditional and links for articles on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
-require 'open-uri'
-require 'json'
-
 class PagesController < ApplicationController
   def home
     @user = User.fetch_or_create(request.remote_ip)
-    @articles = Content.new(@user.profile_id).generate.articles
-    #Remember that content has articles, profile_id, and theme
+    
+    content = Content.new(@user.profile_id).generate
+    @articles = content.articles
+    @theme = content.theme
+    #content has articles, profile_id, and theme
   end
 end
